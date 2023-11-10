f74d76dbffe471f8cdd655ee9934f5bb50bdae00
hirohata <hirohatatro@gmail.com>
2023-11-10 14:51:29 -0800

    feature: added tests

--- test/resultType.test.ts
+++ test/resultType.test.ts
@@ -53,29 +53,77 @@ describe("ERR", () => {
 });
 
 describe("result", () => {
-	const okResult = (): Result<string, any> => {
-		return Ok("value");
-	};
 
-	const errResult = (): Result<any, Error> => {
-		return Err(new Error("error"));
-	};
+	describe("ok", () => {
 
-	it("ok", () => {
+		const VALUE = "value"
+
+		const okResult = (): Result<string, any> => {
+			return Ok(VALUE);
+		};
+
 		const ok = okResult();
 
-		// to access to value, it needs type guard
-		if (ok.ok) {
-			expect(ok.value).toBe("value");
-		}
-	});
+		it("how to use ok", () => {
+			// to access to value, it needs type guard
+			if (ok.ok) {
+				expect(ok.value).toBe("value");
+			}
 
-	it("err", () => {
-		const err = errResult();
+		})
 
-		// to access to error, it needs type guard
-		if (!err.ok) {
-			expect(err.error).toBeInstanceOf(Error);
-		}
-	});
+		it("unwrap in ok", () => {
+			const unwrapResult = okResult().unwrap();
+			expect(unwrapResult).toBe(VALUE);
+		})
+
+		it("unwrap or default in ok", () => {
+			const DEFAULT = "default";
+			const unwrapOrElseResult = okResult().unwrapOrElse(DEFAULT);
+			expect(unwrapOrElseResult).toBe(VALUE);
+			expect(unwrapOrElseResult).not.toBe(DEFAULT);
+		})
+
+		it("unwrap err in ok", () => {
+			expect(() => okResult().unwrapError()).toThrow();
+		});
+
+	})
+
+	describe("error", () => {
+
+		const ERROR = new Error("error")
+
+		const errResult = (): Result<any, Error> => {
+			return Err(ERROR);
+		};
+
+		it("err", () => {
+			const err = errResult();
+
+			// to access to error, it needs type guard
+			if (!err.ok) {
+				expect(err.error).toBeInstanceOf(Error);
+			}
+		});
+
+		it("unwrap in err", () => {
+			expect(() => errResult().unwrap()).toThrowError()
+		})
+
+		it("unwrap or default in err", () => {
+			const DEFAULT = "default";
+			const unwrapOrElseResult = errResult().unwrapOrElse(DEFAULT);
+			expect(unwrapOrElseResult).toBe(DEFAULT);
+		})
+
+		it("unwrap err in ok", () => {
+			expect(errResult().unwrapError()).toBeInstanceOf(Error);
+		});
+
+	})
+
+
+
+
 });
